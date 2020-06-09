8f253f456a7ed8b1911d3d5f7779fcc0d2144b19
Cooper Timmerman <coopert925@gmail.com>
2020-06-09 13:00:43 -0700

    quick smilesnap code change

--- dist/index.php
+++ dist/index.php
@@ -61,6 +61,8 @@
     window.__smilesnap = window.__smilesnap || [];
     window.__smilesnap.cid = 'GSpGRfKWmKWqqwAAqSw2y5Gu';
     window.__smilesnap.fullscreenSelector = '#smilesnap-expanded-container';
+    window.__smilesnap.forceOpen = true;
+
     (function() {
     var ss = document.createElement('script'); ss.type = 'text/javascript'; ss.async = true;
     ss.src = 'https://app-cdn.smilesnap.com/js/loader.js';
