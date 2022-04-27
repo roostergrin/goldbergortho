239ce5f416743582726b51e9a424d9be6ce29c35
GreenWebMedia <37212563+GreenWebMedia@users.noreply.github.com>
2022-04-27 15:26:13 +0530

    Update appointment-request-form.vue

--- src/components/form/appointment-request-form/appointment-request-form.vue
+++ src/components/form/appointment-request-form/appointment-request-form.vue
@@ -46,7 +46,7 @@ export default {
         .then(res => {
           this.formSuccess = true
           setTimeout(() => {
-            window.location.href = 'http://goldbergortho.rgwplogin.com/thank-you'
+            window.location.href = 'https://goldbergortho.com/thank-you'
           }, 400)
           setTimeout(() => {
             this.yourName = ''
